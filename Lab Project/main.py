c3ecd6ba7cc821637e51d2d01ac72454f9573087
Aryan-401 <aryan.raj.garg@gmail.com>
2022-07-23 22:49:49 +0530

    Understood the basics of Grid Layout

--- Lab Project/main.py
+++ Lab Project/main.py
@@ -1,11 +1,22 @@
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.orientation = 'lr-tb'
+        for i in range(10):
+            # size = dp(100 + i * 10)
+            size = dp(100)
+            self.add_widget(Button(text=str(i+1), size_hint=(None, None), size=(size, size)))
 # class GridLayoutExample(GridLayout):
 #     pass
 
