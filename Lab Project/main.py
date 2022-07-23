c19991f925b29971d626c3c583cf55d9e8be1eb4
Aryan-401 <aryan.raj.garg@gmail.com>
2022-07-23 15:20:54 +0530

    Understood the basics of Anchor Layout

--- Lab Project/main.py
+++ Lab Project/main.py
@@ -2,7 +2,12 @@ from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
+
+
+# class GridLayoutExample(GridLayout):
+#     pass
 
 
 class AnchorLayoutExample(AnchorLayout):
